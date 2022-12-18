35f2e108d5b09888c9686f6e95c038279b887286
carlosgaspari1998 <80797135+carlosgaspari1998@users.noreply.github.com>
2022-12-18 20:58:50 -0300

    Add files via upload

---
+++ go/script.sql
@@ -0,0 +1,16 @@
+create database monitoring
+
+CREATE TABLE sites (
+	id int NOT NULL AUTO_INCREMENT,
+	url varchar(100) NOT NULL,
+    frequency float NOT NULL,
+    last_execution_date datetime,
+    sucess bool,
+    response_time float,
+    response_average_time float,
+    creation_date datetime NOT NULL,
+    last_updated_date datetime,
+    PRIMARY KEY (id)
+)
+
+insert into sites (url, frequency, creation_date) values ('https://google.com', 2, '2022-11-23 17:31:26')
